4077af70bb3ac0dc5d5addcbbf3c4ca6346e6041
Zegs <zegrour757@gmail.com>
2020-06-14 19:27:37 -0400

    casing for file

--- template/src/Config/index.ts
+++ template/src/Config/index.ts
@@ -1,4 +1,4 @@
 export { breakPoints } from './breakPoints';
 export { GlobalStyle } from './globalStyle';
-export { GraphQLProvider } from './graphQLProvider';
+export { GraphQLProvider } from './GraphQLProvider';
 export { theme } from './theme';
