e42482e6b5c8dae2a067ded36d2003c73b8cde03
Zegs21 <zegrour757@gmail.com>
2021-04-18 12:02:24 -0400

    removed material-ui references

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,9 +2,5 @@ module.exports = {
   extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-restricted-imports': [
-      'error',
-      { patterns: ['@material-ui/*/*/*', '!@material-ui/core/test-utils/*'] },
-    ],
   },
 };
